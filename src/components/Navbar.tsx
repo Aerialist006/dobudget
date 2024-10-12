ff89caf7f2c11e009a26b52daaf878ad0ce38721
Aerialist006 <ljairolopez@gmail.com>
2024-10-12 16:48:37 -0400

    Added Footer (no final version)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <div className="flex bg-white h-16 md:h-20 items-center px-6 lg:px-36 md:px-20 justify-between w-screen fixed z-10 ">
+    <div className="flex bg-white h-16 md:h-18 items-center px-6 lg:px-36 md:px-20 justify-between w-screen fixed z-10 ">
       <div>hola</div>
       <div className="flex gap-10">
         <div>Menu1</div>
